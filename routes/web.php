db8fc5223b67ac9c467767b14afaa9a01d8bb344
Anna1808v <voskoboyeva18@gmail.com>
2022-12-23 17:46:21 +0200

    front page

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Main'], function(){
+    Route::get('/', 'IndexController');
 });
+
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
