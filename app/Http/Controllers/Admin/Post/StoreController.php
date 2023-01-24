311aafad42ed7b3556664f8317ceba5741c01180
Anna1808v <voskoboyeva18@gmail.com>
2023-01-24 20:19:51 +0200

    adding images to a post

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Post;
 
 use App\Post;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\Admin\Post\StoreRequest;
 
 class StoreController extends Controller
@@ -11,6 +12,9 @@ class StoreController extends Controller
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
+        $data['preview_image'] = Storage::put('/images', $data['preview_image']);
+        $data['main_image'] = Storage::put('/images', $data['main_image']);
+        
         Post::firstOrCreate($data);
         return redirect()->route('admin.post.index');
     }
--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -63,25 +63,16 @@
 
 <!-- jQuery -->
 <script src="{{ asset('plugins/jquery/jquery.min.js') }} "></script>
-<!-- jQuery UI 1.11.4 -->
 <script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }} "></script>
-
 <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }} "></script>
-
 <script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }} "></script>
-
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
-<!-- daterangepicker -->
 <script src="{{ asset('plugins/moment/moment.min.js') }} "></script>
 <script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }} "></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<!-- Summernote -->
-<!-- overlayScrollbars -->
+<script src="{{ asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }} "></script>
 <script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }} "></script>
-<!-- AdminLTE App -->
 <script src="{{ asset('dist/js/adminlte.js') }} "></script>
 <script>
   $(document).ready(function() {
@@ -96,8 +87,9 @@
       ]
     });
   });
+  $(function () {
+    bsCustomFileInput.init();
+  });
 </script>
-<!-- AdminLTE for demo purposes -->
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 </body>
 </html>
