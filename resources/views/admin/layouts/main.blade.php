25bbce6d12bc49d5571fec8603cc7eb2b1921b64
Anna1808v <voskoboyeva18@gmail.com>
2023-01-26 18:45:09 +0200

    link a post to a tag

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -7,18 +7,15 @@
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }} ">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }} ">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bootstrap 4 -->
-  <!-- iCheck -->
-  <!-- JQVMap -->
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }} ">
 
   <link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.min.css') }} ">
-  
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="{{ asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }} ">
   <!-- Daterange picker -->
@@ -65,6 +62,7 @@
 <script src="{{ asset('plugins/jquery/jquery.min.js') }} "></script>
 <script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }} "></script>
 <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }} "></script>
+<script src="{{ asset('plugins/select2/js/select2.full.min.js') }} "></script>
 <script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }} "></script>
 <script>
   $.widget.bridge('uibutton', $.ui.button)
@@ -91,6 +89,8 @@
   $(function () {
     bsCustomFileInput.init();
   });
+
+  $('.select2').select2()
 </script>
 <style>
   .custom-file-input:lang(en)~.custom-file-label::after {
