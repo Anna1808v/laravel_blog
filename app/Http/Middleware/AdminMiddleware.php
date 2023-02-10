d08dc8803efedfa529eba403d19692de100f90d7
Anna1808v <voskoboyeva18@gmail.com>
2023-02-10 20:19:45 +0200

    Middleware 2

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\User;
 
 class AdminMiddleware
 {
@@ -15,6 +16,9 @@ class AdminMiddleware
      */
     public function handle($request, Closure $next)
     {
+        if((int) auth()->user()->role !== User::ROLE_ADMIN){
+            abort(404);
+        }
         return $next($request);
     }
 }
