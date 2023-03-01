84a9a0077bb494e4fe43ee6d184b0f9aabeda8a2
Anna1808v <voskoboyeva18@gmail.com>
2023-03-01 18:11:15 +0200

    post feed, pagination, popular posts, random posts

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use App\Tag;
+use App\User;
+use App\Category;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -15,5 +17,15 @@ class Post extends Model
     public function tags()
     {
         return $this->belongsToMany(Tag::class, 'post_tags', 'post_id', 'tag_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function likedUsers()
+    {
+        return $this->belongsToMany(User::class, 'post_user_likes', 'post_id', 'user_id');
     }
 }
